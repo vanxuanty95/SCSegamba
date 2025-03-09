7590bd7b5421775bcc2893fccd1e96b04346b684
Karl. <63850569+Karl1109@users.noreply.github.com>
2025-03-09 15:47:56 +0800

    code release

--- models/MFS.py
+++ models/MFS.py
@@ -27,8 +27,8 @@ class MFS(nn.Module):
         self.linear_c3 = MLP(input_dim=64, embed_dim=embedding_dim)
         self.linear_c2 = MLP(input_dim=32, embed_dim=embedding_dim)
         self.linear_c1 = MLP(input_dim=16, embed_dim=embedding_dim)
-        self.GSC_C = GBC(embedding_dim*4)
-        self.GSC_8 = GBC(8, norm_type='IN')
+        self.GBC_C = GBC(embedding_dim*4)
+        self.GBC_8 = GBC(8, norm_type='IN')
         self.GN_C = nn.GroupNorm(num_channels=embedding_dim*4, num_groups=embedding_dim*4//16)
         self.linear_fuse = BottConv(embedding_dim*4, embedding_dim, embedding_dim//8, kernel_size=1, padding=0, stride=1)
 
@@ -57,7 +57,7 @@ class MFS(nn.Module):
         b, c, h, w = c1.shape
         out_c1 = self.linear_c1(c1.reshape(b, c, h*w).permute(0, 2, 1)).permute(0, 2, 1).reshape(b, self.embedding_dim, h, w)
 
-        out_c = self.GSC_C(torch.cat([out_c4, out_c3, out_c2, out_c1], dim=1))
+        out_c = self.GBC_C(torch.cat([out_c4, out_c3, out_c2, out_c1], dim=1))
         out_c = self.linear_fuse(out_c)
 
         out_c = self.dropout(out_c)
