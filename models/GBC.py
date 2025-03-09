15a1ff40813609d08a478a8bd8d9fb5dca34beeb
Karl. <63850569+Karl1109@users.noreply.github.com>
2025-03-09 15:45:39 +0800

    Update

--- models/GBC.py
+++ models/GBC.py
@@ -4,7 +4,7 @@ Github: https://github.com/Karl1109
 Email: liuhui@ieee.org
 '''
 
-from torch import nn as nn
+import torch.nn as nn
 
 class BottConv(nn.Module):
     def __init__(self, in_channels, out_channels, mid_channels, kernel_size, stride=1, padding=0, bias=True):
@@ -19,53 +19,49 @@ class BottConv(nn.Module):
         x = self.pointwise_2(x)
         return x
 
+
+def get_norm_layer(norm_type, channels, num_groups):
+    if norm_type == 'GN':
+        return nn.GroupNorm(num_groups=num_groups, num_channels=channels)
+    else:
+        return nn.InstanceNorm3d(channels)
+
+
 class GBC(nn.Module):
-    def __init__(self, in_channels, norm_type = 'GN') -> None:
-        super().__init__()
+    def __init__(self, in_channels, norm_type='GN'):
+        super(GBC, self).__init__()
 
-        self.proj = BottConv(in_channels, in_channels, in_channels//8, 3, 1, 1)
-        self.norm = nn.InstanceNorm3d(in_channels)
-        if(norm_type == 'GN'):
-            self.norm = nn.GroupNorm(num_channels=in_channels, num_groups=in_channels//16)
-        self.nonliner = nn.ReLU()
+        self.block1 = nn.Sequential(
+            BottConv(in_channels, in_channels, in_channels // 8, 3, 1, 1),
+            get_norm_layer(norm_type, in_channels, in_channels // 16),
+            nn.ReLU()
+        )
 
-        self.proj2 = BottConv(in_channels, in_channels, in_channels//8, 3, 1, 1)
-        self.norm2 = nn.InstanceNorm3d(in_channels)
-        if(norm_type == 'GN'):
-            self.norm2 = nn.GroupNorm(num_channels=in_channels, num_groups=in_channels//16)
-        self.nonliner2 = nn.ReLU()
+        self.block2 = nn.Sequential(
+            BottConv(in_channels, in_channels, in_channels // 8, 3, 1, 1),
+            get_norm_layer(norm_type, in_channels, in_channels // 16),
+            nn.ReLU()
+        )
 
-        self.proj3 = BottConv(in_channels, in_channels, in_channels//8, 1, 1, 0)
-        self.norm3 = nn.InstanceNorm3d(in_channels)
-        if(norm_type == 'GN'):
-            self.norm3 = nn.GroupNorm(num_channels=in_channels, num_groups=in_channels//16)
-        self.nonliner3 = nn.ReLU()
+        self.block3 = nn.Sequential(
+            BottConv(in_channels, in_channels, in_channels // 8, 1, 1, 0),
+            get_norm_layer(norm_type, in_channels, in_channels // 16),
+            nn.ReLU()
+        )
 
-        self.proj4 = BottConv(in_channels, in_channels, in_channels//8, 1, 1, 0)
-        self.norm4 = nn.InstanceNorm3d(in_channels)
-        if(norm_type == 'GN'):
-            self.norm4 = nn.GroupNorm(num_channels=in_channels, num_groups=16)
-        self.nonliner4 = nn.ReLU()
+        self.block4 = nn.Sequential(
+            BottConv(in_channels, in_channels, in_channels // 8, 1, 1, 0),
+            get_norm_layer(norm_type, in_channels, 16),
+            nn.ReLU()
+        )
 
     def forward(self, x):
-        x_residual = x
+        residual = x
 
-        x1_1 = self.proj(x)
-        x1_1 = self.norm(x1_1)
-        x1_1 = self.nonliner(x1_1)
-
-        x1 = self.proj2(x1_1)
-        x1 = self.norm2(x1)
-        x1 = self.nonliner2(x1)
-
-        x2 = self.proj3(x)
-        x2 = self.norm3(x2)
-        x2 = self.nonliner3(x2)
-
+        x1 = self.block1(x)
+        x1 = self.block2(x1)
+        x2 = self.block3(x)
         x = x1 * x2
-        x = self.proj4(x)
-        x = self.norm4(x)
-        x = self.nonliner4(x)
+        x = self.block4(x)
 
-        return x + x_residual
-
+        return x + residual
